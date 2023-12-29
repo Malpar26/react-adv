3361254804c54f87227fff2dfde82971ad475f1f
malp <109920001+Malpar26@users.noreply.github.com>
2023-12-29 12:43:26 +0100

    component extended styles

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,13 +1,20 @@
-import { createContext } from 'react'
+import { CSSProperties, ReactElement, createContext } from 'react'
 import { useProduct } from '../hooks/useProduct'
 import styles from '../styles/styles.module.css'
 
-import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces'
+import { ProductContextProps, Product } from '../interfaces/interfaces'
 
 export const ProductContext = createContext({} as ProductContextProps)
 const {Provider} = ProductContext
 
-export function ProductCard({children, product}: ProductCardProps) {
+export interface Props {
+  product: Product
+  children?: ReactElement | ReactElement[]
+  className?: string
+  style?: CSSProperties
+}
+
+export function ProductCard({children, product, className, style}: Props) {
   const {counter, increaseBy} = useProduct()
 
   return (
@@ -16,7 +23,10 @@ export function ProductCard({children, product}: ProductCardProps) {
       counter,
       increaseBy
     }}>
-      <div className={styles.productCard}>
+      <div 
+        className={`${styles.productCard} ${className}`}
+        style={style}
+      >
 
         {children}
 
--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,9 +1,15 @@
-import { useContext } from 'react'
+import { CSSProperties, useContext } from 'react'
 import { ProductContext } from './ProductCard'
 import styles from '../styles/styles.module.css'
 import noImage from '../assets/no-image.jpg'
 
-export function ProductImage({img= ''}) {
+export interface Props {
+  img?: string
+  className?: string
+  style?: CSSProperties
+}
+
+export function ProductImage({img= '', className, style}: Props) {
   const {product} = useContext(ProductContext)
   let imgToShow: string
 
@@ -19,7 +25,8 @@ export function ProductImage({img= ''}) {
     <img
         src={imgToShow}
         alt='Product'
-        className={styles.productImg}
+        className={`${styles.productImg} ${className}`}
+        style={style}
       />
   )
 }
